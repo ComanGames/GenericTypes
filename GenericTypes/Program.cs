e7bc6cee641e7b32cf7c733d7f2c9f3557c64be9
ComanGames <alfaa.gen@gmail.com>
2016-04-19 19:53:44 +0300

    Last of lest changes

--- GenericTypes/Program.cs
+++ GenericTypes/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericTypes
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int[] A = new[] {5, 8,10,12,15,5};
             A = RemoveNumberFromArray<int>(A, 12);
@@ -17,11 +14,44 @@ namespace GenericTypes
             B = RemoveNumberFromArray<string>(B, "B");
             Console.WriteLine("Your are is:");
             ConsoleWriteArray(A);
-            Console.WriteLine();
             ConsoleWriteArray(B);
+            List<Cow> list = new List<Cow>();
+            Cow cow = new Cow("Tomi");
+            list.Add(cow);
+
+            Cow[] cows = new[] {new Cow("Molli"), new Cow("Bob"), new Cow("Tomi")};
+            ConsoleWriteArray(cows);
+            Console.WriteLine("Now we remove cow name TomI");
+            cows = RemoveNumberFromArray(cows, new Cow("Tomi"));
+            ConsoleWriteArray(cows);
+            Console.WriteLine("HashCode");
+            Cow cow1 = new Cow("Moli");
+            Cow cow2 = new Cow("Moli");
+            int a = 5;
+            Console.WriteLine($"cow1 ={cow1.GetHashCode()}");
+            Console.WriteLine($"cow2 ={cow2.GetHashCode()}");
+            Console.WriteLine($"a ={a.GetHashCode()}");
             Console.ReadKey();
 
 
+
+
+            /*
+            //Second example
+            int sc = MethodCreate<int>();
+            string sb = MethodCreate<string>();
+            char c = MethodCreate<char>();
+
+            Console.WriteLine($"Type of sc{sc}");
+            Console.WriteLine($"Type of sb {sb}");
+            Console.WriteLine($"Type of c {c}");
+            Console.ReadKey();
+            */
+        }
+
+        private static T MethodCreate<T>()
+        {
+            return default(T);
         }
 
         private static void ConsoleWriteArray<T>(T[] A)
@@ -31,7 +61,7 @@ namespace GenericTypes
             {
                 Console.Write(A[i] + ",");
             }
-            Console.Write("}");
+            Console.WriteLine("}");
         }
 
         private static T[] RemoveNumberFromArray<T>(T[] inputArrayInts, T numberToRemove)
@@ -40,5 +70,31 @@ namespace GenericTypes
             list.Remove(numberToRemove);
             return list.ToArray();
         }
+    }
+
+    internal class Cow
+    {
+        public string Name;
+
+        public Cow(string molli)
+        {
+            Name = molli;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is Cow)
+                return Name == (obj as Cow).Name;
+            return this.GetHashCode() == obj.GetHashCode();
+        }
+    }
+
+    internal class SomeClass    
+    {
     }
 }
